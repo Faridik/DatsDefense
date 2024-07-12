171a116636ae50b72562f2966af6bcb95659ba9f
Faridik <mifa_98@mail.ru>
2024-07-13 02:15:07 +0300

    Solve: building algorithm by pattern

--- algorithms/base.py
+++ algorithms/base.py
@@ -3,14 +3,19 @@ import os
 
 class Base:
     def __init__(self, priority_building_file='dens5.txt') -> None:
-        self._priority_building_file = priority_building_file
-        self._curr_len_of_priority = 10
+        
+        # Загрузка паттерна
+        with open(os.path.join("algorithms", "build_patterns", priority_building_file)) as f:
+            self._priority_building = json.load(f)   
+        
+        # Примерная длина, до которой можем достроиться
+        self._curr_len_of_priority = 3
 
 
     def update(self, units, world, head):
-        return self.update_with_pattern(units, world, head)
+        return self._update_with_pattern(units, world, head)
 
-    def update_with_pattern(self, units, world, head):
+    def _update_with_pattern(self, units, world, head):
         """
         Строит по возможности клетки баз, проходясь по 
         списку priority_building
@@ -22,21 +27,20 @@ class Base:
         gold = units['player']['gold']
         if gold == 0:
             return []
+        
+        # Теоретический максимум: предыдущий шаг + кол-во монет
+        self._curr_len_of_priority += gold
 
         # Что может помешать
         our_base = units.get("base", []) or []
         zombies = units.get("zombies", []) or []
-        anPlayerBases = units.get("enemyBlocks", []) or []
+        an_player_bases = units.get("enemyBlocks", []) or []
         zposts = world.get("zpots", []) or []
 
         # Список для постройки
         to_build = []
 
-        # Подгрузить список приоритета
-        with open(os.path.join("build_patterns", self._priority_building_file)) as f:
-            priority_building = json.load(f)
-
-        for i, next_coords in enumerate(priority_building):
+        for i, next_coords in enumerate(self._priority_building):
 
             if i > self._curr_len_of_priority:
                 break
@@ -49,11 +53,15 @@ class Base:
                 if x == base['x'] and y == base['y']:
                     continue
 
-            # TODO Можно ли тут построить
-
-
-            # TODO Обновления длины
-
+            # Можно ли тут построить
+            can_be_build = False
+            for base in our_base:
+                if x >= base['x'] - 1 and x <= base['x'] + 1 and \
+                    y >= base['y'] - 1 and y <= base['y'] + 1:
+                    can_be_build = True
+                    continue
+            if not can_be_build:
+                continue
 
             # Если зомби
             for zombie in zombies:
@@ -61,12 +69,12 @@ class Base:
                     continue
 
             # Если клетка базы игрока
-            for anPlayerBase in anPlayerBases:
-                if x >= anPlayerBase['x'] - 1 and x <= anPlayerBase['x'] + 1 and \
-                        y >= anPlayerBase['y'] - 1 and y <= anPlayerBase['y'] + 1:
+            for an_player_base in an_player_bases:
+                if x >= an_player_base['x'] - 1 and x <= an_player_base['x'] + 1 and \
+                        y >= an_player_base['y'] - 1 and y <= an_player_base['y'] + 1:
                     continue
 
-            # Блок базы нельзя ставить вплотную к клетке спота зомби
+            # Блок базы нельзя ставить вплотную к клетке спота зомби или стены
             for zpost in zposts:
                 distance = abs(zpost['x'] - x) + abs(zpost['y'] - y)
                 if distance <= 1:
@@ -79,5 +87,8 @@ class Base:
             # Если золота нет
             if gold == 0:
                 break
+
+        # Обновления длины
+        self._curr_len_of_priority = i
 
         return to_build
