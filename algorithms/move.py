e263c76eb6d2a057bbd64650075b32c28f156844
Faridik <mifa_98@mail.ru>
2024-07-13 09:31:39 +0300

    Add: move function

--- algorithms/move.py
+++ algorithms/move.py
@@ -1,2 +1,12 @@
-def move():
-    return None
+def move(world, head):
+    zpots = world.get("zpots", []) or []
+    dx = 0
+    dy = 0
+    for zpot in zpots:
+        if zpot["type"] == "default":
+            dx_curr = head["x"] - zpot["x"]
+            dy_curr = head["y"] - zpot["y"]
+            dx += dx_curr
+            dy += dy_curr
+
+    return {"x": dx, "y": dy}
