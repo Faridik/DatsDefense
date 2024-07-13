6bbb5ad09ac14b6b239657285c17274003e8625d
Faridik <mifa_98@mail.ru>
2024-07-13 06:41:44 +0300

    Add: attack algorithm

--- algorithms/attack.py
+++ algorithms/attack.py
@@ -1,2 +1,290 @@
 def attack():
     return 2
+
+class Attack:
+    def __init__(self) -> None:
+        self._zombies = []
+        self._players = []
+        self._our_cells = []
+
+        self._zombie_coeff = {
+            "normal": 1,
+            "fast": 1,
+            "bomber": 5,
+            "liner": 4,
+            "juggernaut": 1,
+            "chaos_knight": 0.5
+        }
+
+        self._response = []
+
+    def update(self, units):
+        return self._attack(units)
+    
+    def _attack(self, units):
+        self._our_cells = units.get("base", []) or []
+        self._zombies = units.get("zombies", []) or []
+        for zombie in self._zombies:
+            zombie['our_cells'] = []
+
+        self._players = units.get("enemyBlocks", []) or []
+        for player in self._players:
+            player['our_cells'] = []
+
+        self._check_enemies()
+        self._set_priority()
+        self._attack_zombie_priority_2()
+        self._attack_other()
+
+        self._zombies = []
+        self._players = []
+        self._our_cells = []
+
+        return self._response
+
+
+    def _check_enemies(self):
+        """
+        Находит всех зомби и игроков в зоне видимости.
+        Создаёт привязку к ячейке базы - противников
+        и наоборот.
+        """
+
+        # Смотрим для каждой ячейки зомби и игроков в её области видимости 
+        # Срез нужен, чтобы работало удаление (это python magic)
+        for cell in self._our_cells[:]:   
+
+            # Привязка клетки к зомби и игрокам
+            cell["zombies"] = []
+            cell["players"] = []
+
+            # Ищем зомби и добавляем ссылки друг на друга
+            for zombie in self._zombies:
+
+                x_len = zombie["x"] - cell["x"]
+                y_len = zombie["y"] - cell["y"]
+                dist = (x_len ** 2 + y_len ** 2) ** (1/2)
+
+                if dist <= cell["range"]:
+                    cell["zombies"].append(zombie)
+                    zombie["our_cells"].append(cell)
+
+            for player in self._players:
+
+                x_len = player["x"] - cell["x"]
+                y_len = player["y"] - cell["y"]
+                dist = (x_len ** 2 + y_len ** 2) ** (1/2)
+
+                if dist <= cell["range"]:
+                    cell["players"].append(player)
+                    player["our_cells"].append(cell)
+
+            # Если зомби рядом нет, то удаляем ячейку        
+            if cell["zombies"] == [] and cell["players"] == []:
+                self._our_cells.remove(cell)
+    
+
+
+    def _set_priority(self):
+        """
+        Задаёт приоритет зомби
+        2 - зомби, которые нас атакуют в этом ходу
+        1 - зомби, которые нас будут атаковать через ход
+        0 - зомби нас не задевает
+        """
+
+        for zombie in self._zombies[:]:
+
+            # Если не можем атаковать, то что ж...
+            if zombie["our_cells"] == []:
+                self._zombies.remove(zombie)
+                continue
+            
+            self._set_priority_for_zombie(zombie)
+
+
+    def _set_priority_for_zombie(self, zombie):
+        # Разбираемся с хаосом
+        # Без картинки не понять, что тут происходит
+        # Да и с картинкой.. простой я сошёл с ума
+        # даа... даа... тут хорошо...
+        if zombie["type"] == "chaos_knight":
+            for cell in zombie["our_cells"]:
+                x_len = abs(zombie["x"] - cell["x"])
+                y_len = abs(zombie["y"] - cell["y"])
+
+                dist = x_len + y_len
+
+                if x_len != 0 or y_len != 0:
+                    if dist == 3:
+                        zombie["priority"] = 2
+                        return
+
+                if dist == 6:
+                    zombie["priority"] = 1
+                    return
+
+                if dist == 2:
+                    zombie["priority"] = 1
+                    return
+
+                if dist == 4 and x_len != 2:
+                    zombie["priority"] = 1
+                    return
+                    
+                zombie["priority"] = 0
+                return
+
+        # Остальные зомби
+        if zombie["direction"] == "right":
+            for cell in zombie["our_cells"]:
+                if zombie["y"] == cell["y"]:
+                    if zombie["x"] <= cell["x"] <= zombie["x"] + zombie["speed"]:
+                        zombie["priority"] = 1 if zombie["waitTurns"] == 1 else 2
+                        return
+                    if zombie["x"] <= cell["x"] <= zombie["x"] + 2 * zombie["speed"]:
+                        zombie["priority"] = 1
+                        return
+
+        if zombie["direction"] == "left":
+            for cell in zombie["our_cells"]:
+                if zombie["y"] == cell["y"]:
+                    if zombie["x"] - zombie["speed"] <= cell["x"] <= zombie["x"]:
+                        zombie["priority"] = 1 if zombie["waitTurns"] == 1 else 2
+                        return
+                    if zombie["x"] - 2 * zombie["speed"] <= cell["x"] <= zombie["x"]:
+                        zombie["priority"] = 1
+                        return
+
+        if zombie["direction"] == "up":
+            for cell in zombie["our_cells"]:
+                if zombie["x"] == cell["x"]:
+                    if zombie["y"] <= cell["y"] <= zombie["y"] + zombie["speed"]:
+                        zombie["priority"] = 1 if zombie["waitTurns"] == 1 else 2
+                        return
+                    if zombie["y"] <= cell["y"] <= zombie["y"] + 2 * zombie["speed"]:
+                        zombie["priority"] = 1
+                        return
+
+        if zombie["direction"] == "down":
+            for cell in zombie["our_cells"]:
+                if zombie["x"] == cell["x"]:
+                    if zombie["y"] - zombie["speed"] <= cell["y"] <= zombie["y"]:
+                        zombie["priority"] = 1 if zombie["waitTurns"] == 1 else 2
+                        return
+                    if zombie["y"] - 2 * zombie["speed"]<= cell["y"] <= zombie["y"]:
+                        zombie["priority"] = 1
+                        return
+
+        zombie["priority"] = 0
+
+
+    def _attack_zombie_priority_2(self):
+        zombies_prior_2 = list(filter(lambda z: z["priority"] == 2, self._zombies))
+
+        for zombie in sorted(zombies_prior_2, 
+                             key=lambda z: -self._zombie_coeff[zombie["type"]] * zombie["attack"]):
+
+            self._attack_one_zombie_prior_2(zombie)
+            self._zombies.remove(zombie)
+
+
+    def _attack_one_zombie_prior_2(self, zombie):
+        for cell in sorted(zombie["our_cells"], 
+                           key=lambda cell: len(cell["zombies"]) + len(cell["players"])):
+
+            # Создаём запрос 
+            self._response.append({
+                "blockId": cell["id"],
+                "target": {
+                    "x": zombie["x"],
+                    "y": zombie["y"]
+                    }
+            })
+            
+            # Удаляем эту ячейку из других зомби
+            for an_zombie in cell["zombies"]:
+                an_zombie["our_cells"].remove(cell)
+
+                # Если в зомби никто не целится, то удаляем его
+                # как будто это не нужно, так как мы больше не итерируем по зомби
+                # if len(an_zombie["our_cells"]) == 0:
+                #     self._zombies.remove(an_zombie)
+            # Можно ещё удалить ссылку на ячейку у игроков
+
+            # Удаляем саму ячейку, больше ей стрелять не сможем
+            self._our_cells.remove(cell)
+
+            # Уменьшаем кол-во жизней
+            zombie["health"] -= cell["attack"]
+
+            if zombie["health"] <= 0:
+                return
+
+    def _attack_other(self):
+        for cell in sorted(self._our_cells, 
+                           key=lambda cell: len(cell["zombies"]) + len(cell["players"])):
+            
+            # Сначала бьём игроков
+            if cell["players"] != []:
+                self._attack_one_enemy(cell, cell["players"][0], "players")
+                continue
+
+            # Затем зомби с приоритетом 1
+            is_attacked = False
+            if cell["zombies"] != []:
+                for zombie in cell["zombies"]:
+                    if zombie["priority"] == 1:
+                        self._attack_one_enemy(cell, zombie, "zombies")
+                        is_attacked = True
+                        break
+            if is_attacked:
+                continue
+            
+            # Затем зомби с приоритетом 0
+            if cell["zombies"] != []:
+                self._attack_one_enemy(cell, cell["zombies"][0], "zombies")
+
+            
+
+
+                            
+            
+    def _attack_one_enemy(self, cell, enemy, type):
+        # Создаём запрос 
+        self._response.append({
+            "blockId": cell["id"],
+            "target": {
+                "x": enemy["x"],
+                "y": enemy["y"]
+                }
+        })
+
+        # Отнимаем хп
+        enemy["health"] -= cell["attack"]
+
+        # Если враг умер, то очищаешь ячейки, которые могут по нему стрелять
+        if enemy["health"] <= 0:
+            for an_cells in enemy["our_cells"]:
+                an_cells[type].remove(enemy)
+        else:   # Если нет, то очищаем только текущю ячейку
+            cell[type].remove(enemy)
+            enemy["our_cells"].remove(cell)
+        
+
+        
+
+            
+
+
+
+
+
+
+
+
+
+
+
+        
+
