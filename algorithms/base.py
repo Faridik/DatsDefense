782822710c86f0238185a16d060e9980a30e2f80
Faridik <mifa_98@mail.ru>
2024-07-13 10:08:40 +0300

    Fix: base

--- algorithms/base.py
+++ algorithms/base.py
@@ -2,18 +2,29 @@ import json
 import os
 
 class Base:
-    def __init__(self, priority_building_file='dens5.txt') -> None:
+    def __init__(self, priority_building_file='dens5_0_0.txt') -> None:
         
         # Загрузка паттерна
         with open(os.path.join("algorithms", "build_patterns", priority_building_file)) as f:
             self._priority_building = json.load(f)   
         
+        self._pattern_x = 0
+        self._pattern_y = 0
         # Примерная длина, до которой можем достроиться
         self._curr_len_of_priority = 3
 
 
     def update(self, units, world, head):
         return self._update_with_pattern(units, world, head)
+    
+    def update_pattern(self, head, move):
+        self._pattern_x = (self._pattern_x + move["x"] - head["x"]) % 6
+        self._pattern_y = (self._pattern_y + move["y"] - head["y"]) % 6
+
+        filename = f"dens5_{self._pattern_x}_{self._pattern_y}.txt"
+        with open(os.path.join("algorithms", "build_patterns", filename)) as f:
+            self._priority_building = json.load(f)  
+
 
     def _update_with_pattern(self, units, world, head):
         """
@@ -49,9 +60,14 @@ class Base:
             y = head['y'] + next_coords[0]
 
             # Если здесь наша база
+            can_be_build = True
             for base in our_base:
                 if x == base['x'] and y == base['y']:
-                    continue
+                    can_be_build = False
+                    break
+            if not can_be_build:
+                continue
+                
 
             # Можно ли тут построить
             can_be_build = False
@@ -64,21 +80,33 @@ class Base:
                 continue
 
             # Если зомби
+            can_be_build = True
             for zombie in zombies:
                 if x == zombie['x'] and y == zombie['y']:
-                    continue
+                    can_be_build = False
+                    break
+            if not can_be_build:
+                continue
 
             # Если клетка базы игрока
+            can_be_build = True
             for an_player_base in an_player_bases:
                 if x >= an_player_base['x'] - 1 and x <= an_player_base['x'] + 1 and \
                         y >= an_player_base['y'] - 1 and y <= an_player_base['y'] + 1:
-                    continue
+                    can_be_build = False
+                    break
+            if not can_be_build:
+                continue
 
             # Блок базы нельзя ставить вплотную к клетке спота зомби или стены
+            can_be_build = True
             for zpost in zposts:
                 distance = abs(zpost['x'] - x) + abs(zpost['y'] - y)
                 if distance <= 1:
-                    continue    
+                    can_be_build = False
+                    break
+            if not can_be_build:
+                continue
 
             # Строим здесь
             to_build.append({'x': x, 'y': y})
