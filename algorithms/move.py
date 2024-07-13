2dcf2fd9b7fa4a3de1dff2a6a2046ade7996067b
Faridik <mifa_98@mail.ru>
2024-07-13 11:56:52 +0300

    fix: /0

--- algorithms/move.py
+++ algorithms/move.py
@@ -13,8 +13,9 @@ def move(units, world, head):
         if zpot["type"] == "default":
             dx_curr = head["x"] - zpot["x"]
             dy_curr = head["y"] - zpot["y"]
-            dx += 1/dx_curr
-            dy += 1/dy_curr
+            dist = ((dx_curr * dx_curr) + (dy_curr * dy_curr)) ** (1/2)
+            dx += dx_curr / dist
+            dy += dy_curr / dist
 
 
     if abs(dy) - abs(dx) > 0:
@@ -25,12 +26,12 @@ def move(units, world, head):
             else:
                 if dx > 0:
                     move["x"] += 1
-                else:
-                    move["x"] -= 1
-                if check_base(units, move):
+                    if not check_base(units, move):
+                        move["y"] += 1
                     return move
                 else:
-                    return None
+                    move["x"] -= 1
+                    return move if check_base(units, move) else None
         else:
             move = {"x": head["x"], "y": head["y"] - 1}
             if check_base(units, move):
